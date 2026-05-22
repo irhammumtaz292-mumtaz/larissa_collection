1347c1899b438048305e46793378139207bd444f
Raven Vanzlysky <mandalaraven@proton.me>
2026-05-22 12:08:04 +0700

    Menambahkan file warna

--- app/admin/admin_warna.php
+++ app/admin/admin_warna.php
@@ -2,56 +2,8 @@
 
     session_start();
 
-    $laman = 'Produk';
+    $laman = 'Warna';
     include '../../assets/layout/admin/header.php';
-
-    // Bahan
-        // Data Akun
-        if(isset($_POST['cari'])) 
-        {
-            $kata_cari = htmlspecialchars(strip_tags($_POST['kata_cari']));
-            $data_bahan = select("SELECT bahan.*, warna.* FROM bahan
-            JOIN warna ON bahan.id_warna = warna.id_warna
-            WHERE CONCAT(jenis_bahan, nama_warna, stok, harga_bahan) LIKE '%$kata_cari%' ORDER BY jenis_bahan ASC, nama_warna ASC");
-        } else 
-        {
-            $data_bahan = select("SELECT bahan.*, warna.* FROM bahan
-            JOIN warna ON bahan.id_warna = warna.id_warna ORDER BY jenis_bahan ASC, nama_warna ASC");
-        }
-
-        // jika tombol tambah di tekan jalankan script berikut
-        if (isset($_POST['tambah'])) {
-            $result = tambah_akun($_POST) > 0;
-
-            $popup = true;
-            $statusPopup = $result ? 'Berhasil' : 'Gagal';
-            $warnaPopup = $result ? 'success' : 'danger';
-            $iconPopup = $result ? 'check2-circle' : 'x-circle';
-            $popupEksekusi = 'Ditambahkan';
-        }
-
-        // jika tombol ubah di tekan jalankan script berikut
-        if (isset($_POST['ubah'])) {
-            $result = ubah_akun($_POST) > 0;
-
-            $popup = true;
-            $statusPopup = $result ? 'Berhasil' : 'Gagal';
-            $warnaPopup = $result ? 'success' : 'danger';
-            $iconPopup = $result ? 'check2-circle' : 'x-circle';
-            $popupEksekusi = 'Diubah';
-        }
-
-        // jika tombol hapus di tekan jalankan script berikut
-        if (isset($_POST['hapus'])) {
-            $result = hapus_akun($_POST) > 0;
-
-            $popup = true;
-            $statusPopup = $result ? 'Berhasil' : 'Gagal';
-            $warnaPopup = $result ? 'success' : 'danger';
-            $iconPopup = $result ? 'check2-circle' : 'x-circle';
-            $popupEksekusi = 'Dihapus';
-        }
-    // .Bahan
 
     // Akun
         // Data Akun
@@ -120,151 +72,6 @@
 
                             <div class="card-wrap d-flex justify-content-between align-items-center">
                                 <h3 class="card-title">Bahan</h3>
-
-                                <form class="form" action="" method="post">
-                                    <div class="input-group">
-                                        <input type="search" class="form-control me-3" name="kata_cari" placeholder="Cari..." aria-label="Search" value="<?php if(isset($_POST['cari'])) { echo $_POST['kata_cari']; } ?>">
-                                        <button class="btn btn-outline-info me-1" type="submit" name="cari"><i class="bi bi-search"></i></button>
-                                    </div>
-                                </form>
-                            </div>
-
-                            <button type="button" class="btn btn-primary btn-sm mb-2" data-bs-toggle="modal" data-bs-target="#modalTambah">
-                            <i class="bi bi-person-plus me-1"></i>
-                                Tambah
-                            </button>  
-
-                            <button class="btn btn-sm btn-secondary mb-2"><i class="bi bi-file-earmark-text me-1"></i>Generate Report</button>
-                            <button class="btn btn-sm btn-success mb-2"><i class="bi bi-download me-1"></i>Export</button>
-
-                        </div>
-                        <!-- /.card-header -->
-
-                        <!-- .card-body -->
-                        <div class="card-body overflow-auto" style="max-height: 400px;">
-
-                            <div class="table-responsive">
-
-                                <table id="table" class="table table-sm table-bordered border-dark table-hover">
-                                <thead class="text-center">
-                                    <tr>
-                                        <th class="bg-success">#</th>
-                                        <th class="bg-success">Jenis Bahan</th>
-                                        <th class="bg-success">Warna</th>
-                                        <th class="bg-success">Stok</th>
-                                        <th class="bg-success">Harga Bahan</th>
-                                        <th class="bg-success">Action</th>
-                                    </tr>
-                                </thead>
-
-                                <tbody>
-                                    <?php $no = 1; ?>
-                                    <?php foreach ($data_bahan as $bahan) : ?>
-                                        <tr style="height: 10px;">
-                                        <td class="text-center"><?= $no++; ?></td>
-                                        <td><?= $bahan['jenis_bahan'] ?></td>
-                                        <td><?= $bahan['nama_warna'] ?></td>
-                                        <td><?= $bahan['stok'] ?></td>
-                                        <td><?= $bahan['harga_bahan'] ?></td>
-                                        <td class="text-center">
-                                        
-                                            <button type="button" class="btn btn-sm btn-outline-primary mb-1" data-bs-toggle="modal" data-bs-target="#modalUbah<?= $bahan['id_bahan'] ?>">
-                                            <i class="bi bi-pen"></i> Ubah
-                                            </button>
-
-                                            <button type="button" class="btn btn-sm btn-outline-danger mb-1" data-bs-toggle="modal" data-bs-target="#modalHapus<?= $bahan['id_bahan'] ?>">
-                                            <i class="bi bi-trash"></i> Hapus
-                                            </button>
-                                            
-                                        </td>
-                                        </tr>
-                                    <?php endforeach; ?>
-                                </tbody>
-                                </table>
-
-                            </div>
-
-                        </div>
-                        <!-- /.card-body -->
-
-                    </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Mobile View -->
-            <div class="d-block d-md-none">
-    
-                <!-- ACTION BUTTON -->
-                <div class="card mb-3 shadow-sm border-0 rounded-4 overflow-hidden">
-
-                    <div class="card-header py-2">
-                        <div class="card-wrap d-flex justify-content-between align-items-center">
-                                <h3 class="card-title"><?= $laman; ?></h3>
-
-                                <form class="form" action="" method="post">
-                                <div class="input-group">
-                                    <input type="search" class="form-control me-3" name="kata_cari" placeholder="Cari..." aria-label="Search" value="<?php if(isset($_POST['cari'])) { echo $_POST['kata_cari']; } ?>">
-                                    <button class="btn btn-outline-info me-1" type="submit" name="cari"><i class="bi bi-search"></i></button>
-                                </div>
-                                </form>
-                            </div>
-                    </div>
-
-                    <div class="card-body p-2">
-
-                        <!-- Tombol utama -->
-                        <button type="button"
-                            class="btn btn-sm btn-primary w-100 rounded-pill fw-semibold shadow-sm mb-2"
-                            data-bs-toggle="modal"
-                            data-bs-target="#modalTambah">
-                            <i class="bi bi-person-plus me-1"></i>
-                            Tambah Akun
-                        </button>
-
-                        <!-- Tombol secondary -->
-                        <div class="row g-2">
-                            <div class="col-6">
-                                <button class="btn btn-sm btn-secondary w-100 rounded-pill">
-                                    <i class="bi bi-file-earmark-text me-1"></i>
-                                    Generate
-                                </button>
-                            </div>
-                            <div class="col-6">
-                                <button class="btn btn-sm btn-success w-100 rounded-pill">
-                                    <i class="bi bi-download me-1"></i>
-                                    Export
-                                </button>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-                <!-- LIST DATA -->
-                <div class="row g-2">
-                    
-                </div>
-
-            </div>
-
-        </section>
-        <!-- .View Data Bahan -->
-
-        <!-- View Data Design -->
-        <section class="mb-3 mb-md-4" aria-label="Design View">
-
-            <!-- Desktop View -->
-            <div class="d-none d-md-block">
-                <div class="row justify-content-center">
-                    <div class="col-sm-6 col-md-8 col-lg-12">
-                    <div class="card">
-
-                        <!-- .card-header -->
-                        <div class="card-header">
-
-                            <div class="card-wrap d-flex justify-content-between align-items-center">
-                                <h3 class="card-title">Design</h3>
 
                                 <form class="form" action="" method="post">
                                 <div class="input-group">
@@ -452,7 +259,7 @@
             </div>
 
         </section>
-        <!-- .View Data Design -->
+        <!-- .View Data Bahan -->
 
     </main>
 
